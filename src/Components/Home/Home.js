41998a51303e1bd7d73b5d0ababf93c7c772e725
leeyang2019 <nyang@madisoncollege.edu>
2020-07-01 13:21:13 -0400

    feat: added playerCards

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Featured from './Featured/Featured';
 import Matches from './Matches/Matches';
+import Players from '../Players/Players';
 
 const Home = () => {
   return (
     <div className="bck_blue">
       <Featured />
       <Matches />
+      <Players />
     </div>
   );
 };
